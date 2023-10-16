acbb7d22ffee6be0e64baff2dd2b1921eee61fb6
NelanJoe <nelanjorelat@gmail.com>
2023-10-16 20:44:48 +0700

    add: feature: page nelan

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Nelan from "./pages/Nelan";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/nelan" element={<Nelan />} />
             </Routes>
         </BrowserRouter>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ const Home = () => {
                     <h1>Fahmi Alfareza</h1>
                     <h1>Hello World</h1>
                     <h2>Coba</h2>
-                    <h1>Hello, From Nelan</h1>
                 </Col>
             </Row>
         </Container>
