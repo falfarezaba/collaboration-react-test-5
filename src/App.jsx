c1fad4f490a1f90d7579162eab8e14dedcd6d86c
falfarezaba <118619819+falfarezaba@users.noreply.github.com>
2023-10-16 21:10:15 +0700

    add: reza page (#5)
    * add: reza page
    
    * add: reza page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Reza from "./pages/Reza";
 import Nelan from "./pages/Nelan";
 import Billy from "./pages/Billy";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/nelan" element={<Nelan />} />
-        <Route path="/billy" element={<Billy />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/reza" element={<Reza />} />
+                <Route path="/nelan" element={<Nelan />} />
+                <Route path="/billy" element={<Billy />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
