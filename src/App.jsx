5151d2a98fbb8411f242dd1dfb1ea19e203a477c
Dwipayana Ismulya <69786097+DwipayanaIsmulya@users.noreply.github.com>
2023-10-16 22:30:25 +0800

    Feature: dwi page (#4)

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Reza from "./pages/Reza";
 import Nelan from "./pages/Nelan";
-import Helmi from "./pages/Helmi";
+import Dwi from "./pages/Dwi";
 import Billy from "./pages/Billy";
+import Helmi from "./pages/Helmi";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/reza" element={<Reza />} />
-                <Route path="/nelan" element={<Nelan />} />
-                <Route path="/helmi" element={<Helmi/>} />
-                <Route path="/billy" element={<Billy />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reza" element={<Reza />} />
+        <Route path="/nelan" element={<Nelan />} />
+        <Route path="/helmi" element={<Helmi />} />
+        <Route path="/billy" element={<Billy />} />
+        <Route path="/dwi" element={<Dwi />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
