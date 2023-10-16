2e8712f4e5d5aa06e2d70bdc5a67c73804b96964
Azka <azkaatqiya4@gmail.com>
2023-10-16 20:57:39 +0700

    add: feature: azka page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Nelan from "./pages/Nelan";
+import Azka from "./pages/Azka";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/nelan" element={<Nelan />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/nelan" element={<Nelan />} />
+        <Route path="/azka" element={<Azka />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
