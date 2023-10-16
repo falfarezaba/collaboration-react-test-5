4c36d036bcd707477a736cb796495eca15268545
NelanJoe <nelanjorelat@gmail.com>
2023-10-16 21:15:28 +0700

    add: page nelan version two

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Home from "./pages/Home";
 import Reza from "./pages/Reza";
 import Nelan from "./pages/Nelan";
 import Billy from "./pages/Billy";
+import NelanTwoVersion from "./pages/NelanTwoVersion";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/reza" element={<Reza />} />
-                <Route path="/nelan" element={<Nelan />} />
-                <Route path="/billy" element={<Billy />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reza" element={<Reza />} />
+        <Route path="/nelan" element={<Nelan />} />
+        <Route path="/nelan-two" element={<NelanTwoVersion />} />
+        <Route path="/billy" element={<Billy />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
