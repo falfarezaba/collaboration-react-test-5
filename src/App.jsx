a417e382c7c92b8bb7b11b5f821616a4f21d8f6c
Muhammad Putra Wibisono <mputrawibisone993@gmail.com>
2023-10-16 22:21:54 +0800

    feature: nambah pages wibi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Nelan from "./pages/Nelan";
-import { Wibi } from "./pages/Wibi";
+import Wibi from "./pages/Wibi";
 
 function App() {
   return (
