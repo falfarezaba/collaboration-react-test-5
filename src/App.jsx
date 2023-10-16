8e063a2af4fccc014e04f94e30d2f4c8855f614e
Fahmi Alfareza <falfareza@binaracademy.org>
2023-10-16 21:05:03 +0700

    add: reza page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Reza from "./pages/Reza";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/reza" element={<Reza />} />
             </Routes>
         </BrowserRouter>
     );
