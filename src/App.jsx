3d341e99beadcbd885a13208254d2939e7fdaca6
Arwin Marinta <89586510+ArwinMarinta@users.noreply.github.com>
2023-10-16 23:02:09 +0800

    Arwin (#11)
    * page arwin
    
    * new
    
    * new 1
    
    * new21
    
    ---------
    
    Co-authored-by: Arwin Marinta <arwinmarinta3@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Reza from "./pages/Reza";
 import Nelan from "./pages/Nelan";
-import Wibi from "./pages/Wibi";
-import Azka from "./pages/Azka";
 import Arwin from "./pages/Arwin";
 import Helmi from "./pages/Helmi";
+import Wibi from "./pages/Wibi";
+import Azka from "./pages/Azka";
 import Dwi from "./pages/Dwi";
 import Billy from "./pages/Billy";
 import NelanTwoVersion from "./pages/NelanTwoVersion";
@@ -17,12 +17,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/reza" element={<Reza />} />
         <Route path="/nelan" element={<Nelan />} />
-        <Route path="/nelan-two" element={<NelanTwoVersion />} />
-        <Route path="/billy" element={<Billy />} />
-        <Route path="/wibi" element={<Wibi />} />
-        <Route path="/helmi" element={<Helmi />} />
-        <Route path="/azka" element={<Azka />} />
         <Route path="/arwin" element={<Arwin />} />
+        <Route path="/helmi" element={<Helmi />} />
+        <Route path="/billy" element={<Billy />} />
+        <Route path="/nelan-two" element={<NelanTwoVersion />} />
+        <Route path="/wibi" element={<Wibi />} />
+        <Route path="/azka" element={<Azka />} />
         <Route path="/dwi" element={<Dwi />} />
       </Routes>
     </BrowserRouter>
