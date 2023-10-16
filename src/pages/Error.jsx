4c96311426ef71a3b1074670fea196fa8e4e63d5
Davincy Project <bdavincy.bd@gmail.com>
2023-10-16 21:42:53 +0700

    feature: add error pages

---
+++ src/pages/Error.jsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div className="min-vh-100 d-flex justify-content-center align-items-center fs-1">
+      404 | Page not Found
+    </div>
+  );
+};
+
+export default Error;
