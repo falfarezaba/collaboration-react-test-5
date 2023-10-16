dd3c586c0a5934da051ac961bf0bcaf64e8e58b5
Dwipayana Ismulya <dwipayanaismulya6@gmail.com>
2023-10-16 21:56:11 +0800

    Feature: dwi page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Nelan from "./pages/Nelan";
+import Dwi from "./pages/Dwi";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/nelan" element={<Nelan />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/nelan" element={<Nelan />} />
+        <Route path="/dwi" element={<Dwi />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
